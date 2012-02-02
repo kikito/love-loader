d24e31982064b7a8f9e39e6eb9f33ec2b494a8eb
Enrique García Cota <kikito@gmail.com>
2012-02-02 23:14:23 +0100

    added support for soundData and imageData. Forward compatibility with LÖVE 0.8.x . Internal renaming

--- love-loader.lua
+++ love-loader.lua
@@ -1,4 +1,4 @@
--- love-loaver v1.0.0 (2012-01)
+-- love-loaver v1.1.0 (2012-02)
 -- Copyright (c) 2011 Enrique García Cota
 -- Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 -- The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
@@ -11,45 +11,68 @@ require "love.sound"
 
 local resourceKinds = {
   image = {
-    inputName   = "imagePath",
-    outputName  = "imageData",
+    requestKey  = "imagePath",
+    resourceKey = "imageData",
     constructor = love.image.newImageData,
     postProcess = function(data)
       return love.graphics.newImage(data)
     end
   },
   source = {
-    inputName   = "sourcePath",
-    outputName  = "source",
-    constructor = love.audio.newSource
+    requestKey  = "sourcePath",
+    resourceKey = "source",
+    constructor = function(path)
+      return love.audio.newSource(path)
+    end
   },
   stream = {
-    inputName   = "streamPath",
-    outputName  = "stream",
+    requestKey  = "streamPath",
+    resourceKey = "stream",
     constructor = function(path)
       return love.audio.newSource(path, "stream")
     end
+  },
+  soundData = {
+    requestKey  = "soundDataPathOrDecoder",
+    resourceKey = "soundData",
+    constructor = love.sound.newSoundData
+  },
+  imageData = {
+    requestKey  = "imageDataPath",
+    resourceKey = "rawImageData",
+    constructor = love.image.newImageData
   }
 }
+
+-- compatibility with LÖVE v0.7.x and 0.8.x
+local function setInThread(thread, key, value)
+  local set = thread.set or thread.send
+  return set(thread, key, value)
+end
+
+local function getFromThread(thread, key)
+  local get = thread.get or thread.receive
+  return get(thread, key)
+end
 
 local producer = love.thread.getThread('loader')
 
 if producer then
 
-  local input, output
+  local requestParam, resource
   local done = false
 
   while not done do
 
     for _,kind in pairs(resourceKinds) do
-      input = producer:receive( kind.inputName )
-      if input then
-        output = kind.constructor( input )
-        producer:send( kind.outputName, output )
+      requestParam = getFromThread(producer, kind.requestKey)
+      if requestParam then
+        resource = kind.constructor(requestParam)
+        setInThread(producer, kind.resourceKey, resource)
       end
     end
 
-    done = producer:receive( "done" )
+    done = getFromThread(producer, "done")
   end
 
 else
@@ -66,42 +89,41 @@ else
     return table.remove(t,1)
   end
 
-  local function newResource(kind, holder, key, input)
+  local function newResource(kind, holder, key, requestParam)
     pending[#pending + 1] = {
-      kind = kind, holder = holder, key = key, input = input
+      kind = kind, holder = holder, key = key, requestParam = requestParam
     }
   end
 
-  local function consumeFromThread(thread)
-    local errorMessage = thread:receive("error")
+  local function getResourceFromThreadIfAvailable(thread)
+    local errorMessage = getFromThread(thread,"error")
     assert(not errorMessage, errorMessage)
 
     local data, resource
     for name,kind in pairs(resourceKinds) do
-      data = thread:receive(kind.outputName)
+      data = getFromThread(thread, kind.resourceKey)
       if data then
         resource = kind.postProcess and kind.postProcess(data, resourceBeingLoaded) or data
         resourceBeingLoaded.holder[resourceBeingLoaded.key] = resource
         loader.loadedCount = loader.loadedCount + 1
-        callbacks.loaded(resourceBeingLoaded.kind, resourceBeingLoaded.holder, resourceBeingLoaded.key)
+        callbacks.oneLoaded(resourceBeingLoaded.kind, resourceBeingLoaded.holder, resourceBeingLoaded.key)
         resourceBeingLoaded = nil
       end
     end
   end
 
-  local function requestToThread(thread)
+  local function requestNewResourceToThread(thread)
     resourceBeingLoaded = shift(pending)
-    local inputName = resourceKinds[resourceBeingLoaded.kind].inputName
-    thread:send(inputName, resourceBeingLoaded.input)
+    local requestKey = resourceKinds[resourceBeingLoaded.kind].requestKey
+    setInThread(thread, requestKey, resourceBeingLoaded.requestParam)
   end
 
-  local function killThreadIfDone(thread)
+  local function endThreadIfAllLoaded(thread)
     if not resourceBeingLoaded and #pending == 0 then
-      thread:send("done", true)
-      callbacks.finished()
+      setInThread(thread,"done",true)
+      callbacks.allLoaded()
     end
   end
-
 
 
   -- public interface starts here
@@ -115,10 +137,18 @@ else
     newResource(kind, holder, key, path)
   end
 
-  function loader.start(finishedCallback, loadedCallback)
+  function loader.newSoundData(holder, key, pathOrDecoder)
+    newResource('soundData', holder, key, pathOrDecoder)
+  end
 
-    callbacks.finished = finishedCallback or function() end
-    callbacks.loaded   = loadedCallback or function() end
+  function loader.newImageData(holder, key, path)
+    newResource('imageData', holder, key, path)
+  end
+
+  function loader.start(allLoadedCallback, oneLoadedCallback)
+
+    callbacks.allLoaded = allLoadedCallback or function() end
+    callbacks.oneLoaded = oneLoadedCallback or function() end
 
     local thread = love.thread.newThread("loader", pathToThisFile)
 
@@ -131,10 +161,10 @@ else
     local thread = love.thread.getThread("loader")
     if thread then
       if resourceBeingLoaded then
-        consumeFromThread(thread)
-        killThreadIfDone(thread)
+        getResourceFromThreadIfAvailable(thread)
+        endThreadIfAllLoaded(thread)
       elseif #pending > 0 then
-        requestToThread(thread)
+        requestNewResourceToThread(thread)
       end
     end
   end
