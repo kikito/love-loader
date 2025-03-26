b675be66dbaaba4a1dc38a26605beb599b25dbe1
Alejandro Alzate <alejandroalzatesanchez@gmail.com>
2025-03-25 21:15:11 -0500

    Add LuaLS annotations and documentation
    Adds detailed annotations for all APIs and types.
    Includes
    documentation for all public facing APIs.

--- love-loader.lua
+++ love-loader.lua
@@ -1,8 +1,36 @@
+
 require "love.filesystem"
 require "love.image"
 require "love.audio"
 require "love.sound"
 
+---@class LoveLoader
+-- Static fields --
+---@field _VERSION string
+---@field _DESCRIPTION string
+---@field _URL string
+---@field _LICENSE string
+-- Functions --
+---@field newImage function
+---@field newFont function
+---@field newBMFont function
+---@field newSource function
+---@field newSoundData function
+---@field newImageData function
+---@field newCompressedData function
+---@field rawData function
+---@field newVideo function
+---@field read function
+---@field start function
+---@field update function
+-- Dynamic fields --
+---@field loadedCount? number
+---@field resourceCount? number
+---@field thread? love.Thread
+
+-- Due to the if statement LuaLS will think that the fields are missing.
+---@type LoveLoader
+---@diagnostic disable-next-line: missing-fields
 local loader = {
   _VERSION     = 'love-loader v2.0.3',
   _DESCRIPTION = 'Threaded resource loading for LÖVE',
@@ -33,6 +61,25 @@ local loader = {
   ]]
 }
 
+
+---@alias ResourceKind string   # The kind of resource to load
+---| "newImage" string          # Load an Image
+---| "newFont" string           # Load a Font
+---| "newBMFont" string         # Load a Bitmap Font
+---| "newSource" string         # Load a Source
+---| "newSoundData" string      # Load a SoundData
+---| "newImageData" string      # Load an ImageData
+---| "newCompressedData" string # Load a CompressedData
+---| "rawData" string           # Load raw text data
+---| "newVideo" string          # Load a Video
+
+---@class ResourceKindConstructor
+---@field requestKey string
+---@field resourceKey string
+---@field constructor fun(path: string): any, ...
+---@field postProcess? fun(data: any, resource: any): any
+
+---@type table<ResourceKind, ResourceKindConstructor>
 local resourceKinds = {
   image = {
     requestKey  = "imagePath",
@@ -78,6 +125,7 @@ local resourceKinds = {
     postProcess = function(data, resource)
       local imagePath, glyphsPath  = unpack(resource.requestParams)
       local glyphs = love.filesystem.newFileData(glyphsPath)
+      ---@diagnostic disable-next-line: param-type-mismatch
       return love.graphics.newFont(glyphs,data)
     end
   },
@@ -127,8 +175,8 @@ if loaded == true then
   while not done do
 
     for _,kind in pairs(resourceKinds) do
-      local loader = love.thread.getChannel(CHANNEL_PREFIX .. kind.requestKey)
-      requestParams = loader:pop()
+      local loaderChannel = love.thread.getChannel(CHANNEL_PREFIX .. kind.requestKey)
+      requestParams = loaderChannel:pop()
       if requestParams then
         resource = kind.constructor(unpack(requestParams))
         local producer = love.thread.getChannel(CHANNEL_PREFIX .. kind.resourceKey)
@@ -191,39 +239,85 @@ else
 
   -----------------------------------------------------
 
+  --- Loads a Image
+  ---@param holder table The table where the image will be stored.
+  ---@param key string The key to store the image under.
+  ---@param path string The path to the image file.
+  ---@return nil
   function loader.newImage(holder, key, path)
     newResource('image', holder, key, path)
   end
 
+  --- Loads a Font
+  ---@param holder table The table where the font will be stored.
+  ---@param key string The key to store the font under.
+  ---@param path string The path to the font file.
+  ---@param size? number The size of the font.
+  ---@return nil
   function loader.newFont(holder, key, path, size)
     newResource('font', holder, key, path, size)
   end
 
+  --- Loads a Bitmap Font
+  ---@param holder table The table where the font will be stored.
+  ---@param key string The key to store the font under.
+  ---@param path string The path to the font file.
+  ---@param glyphsPath? string The path to the glyphs file.
+  ---@return nil
   function loader.newBMFont(holder, key, path, glyphsPath)
     newResource('font', holder, key, path, glyphsPath)
   end
 
+  --- Loads an audio source
+  ---@param holder table The table where the audio source will be stored.
+  ---@param key string The key to store the audio source under.
+  ---@param path string The path to the audio file.
+  ---@param sourceType? love.SourceType The type of audio source to load.
+  ---@return nil
   function loader.newSource(holder, key, path, sourceType)
     local kind = (sourceType == 'static' and 'staticSource' or 'streamSource')
     newResource(kind, holder, key, path)
   end
 
+  --- Loads a sound as a raw SoundData object
+  ---@param holder table The table where the sound will be stored.
+  ---@param key string The key to store the sound under.
+  ---@param pathOrDecoder? any The path to the sound file or a decoder function.
   function loader.newSoundData(holder, key, pathOrDecoder)
     newResource('soundData', holder, key, pathOrDecoder)
   end
 
+  --- Loads an image as an raw ImageData object
+  ---@param holder table The table where the imageData will be stored.
+  ---@param key string The key to store the imageData under.
+  ---@param path string The path to the image file.
+  ---@return nil
   function loader.newImageData(holder, key, path)
     newResource('imageData', holder, key, path)
   end
 
+  --- Loads a newCompressedData from an image
+  ---@param holder table The table where the compressedData will be stored.
+  ---@param key string The key to store the compressedData under.
+  ---@param path string The path to the image file.
+  ---@return nil
   function loader.newCompressedData(holder, key, path)
     newResource('compressedData', holder, key, path)
   end
 
+  --- Useful to read raw data, such as text.
+  ---@param holder table The table where the rawData will be stored.
+  ---@param key string The key to store the rawData under.
+  ---@param path string The path to the file.
+  ---@return nil
   function loader.read(holder, key, path)
     newResource('rawData', holder, key, path)
   end
 
+  --- Starts the asset loading process on another thread.
+  ---@param allLoadedCallback function Callback function that is called when all assets are loaded
+  ---@param oneLoadedCallback function Callback function that is called when each asset is loaded
+  ---@return nil
   function loader.start(allLoadedCallback, oneLoadedCallback)
 
     callbacks.allLoaded = allLoadedCallback or function() end
@@ -237,6 +331,11 @@ else
     loader.thread = thread
   end
 
+  --- Check the state of the worker thread.
+  --- - It makes sure that the worker thread is running, raises an error if applicable.
+  --- - Transfers asset pointers to the main thread.
+  --- - It doesn't use delta time to function.
+  ---@return nil
   function loader.update()
     if loader.thread then
       if loader.thread:isRunning() then
